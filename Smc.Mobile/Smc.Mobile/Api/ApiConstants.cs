4a60e46b09f860b6b8ade99a9084e54e84b6d876
danilo <danilo.mendez@gmail.com>
2018-11-27 17:57:51 -0500

    ...

--- Smc.Mobile/Smc.Mobile/Api/ApiConstants.cs
+++ Smc.Mobile/Smc.Mobile/Api/ApiConstants.cs
@@ -6,8 +6,11 @@ namespace Smc.Mobile.Api
 {
     public class ApiConstants
     {
+#if DEBUG
         public static readonly string Baseurl = "http://192.168.2.2/apipublic/";
-        // public static readonly string Baseurl = "https://smc.dyteks.com/apipublic/";
+#else
+        public static readonly string Baseurl = "https://smc.dyteks.com/apipublic/";
+#endif
         // public static readonly string AuthHeaderName = "Authorization";
 
         public const string ZipCode = @"zipcode?zip=";
