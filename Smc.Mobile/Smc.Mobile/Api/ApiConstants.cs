fe1f086846ad2a08251227b5fe4d819bfce2c03f
danilo <danilo.mendez@gmail.com>
2018-11-28 17:12:05 -0500

    Arreglos de botones

--- Smc.Mobile/Smc.Mobile/Api/ApiConstants.cs
+++ Smc.Mobile/Smc.Mobile/Api/ApiConstants.cs
@@ -6,8 +6,8 @@ namespace Smc.Mobile.Api
 {
     public class ApiConstants
     {
-        public static readonly string Baseurl = "http://192.168.1.26/apipublic/";
-        // public static readonly string Baseurl = "https://smc.dyteks.com/apipublic/";
+        //public static readonly string Baseurl = "http://192.168.1.26/apipublic/";
+        public static readonly string Baseurl = "https://smc.dyteks.com/apipublic/";
         // public static readonly string AuthHeaderName = "Authorization";
 
         public const string ZipCode = @"zipcode?zip=";
